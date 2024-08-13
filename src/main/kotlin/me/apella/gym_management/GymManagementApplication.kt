0fa8b54582a8123f940d419640a1ed4df8e06e98
apella1 <johncollons5@gmail.com>
2024-08-13 08:17:10 +0300

    set up projects

---
+++ src/main/kotlin/me/apella/gym_management/GymManagementApplication.kt
@@ -0,0 +1,11 @@
+package me.apella.gym_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<GymManagementApplication>(*args)
+}
---
+++ src/test/kotlin/me/apella/gym_management/GymManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.apella.gym_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GymManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
